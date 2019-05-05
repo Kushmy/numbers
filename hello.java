39b1dae0aa1c18c74fa04111da4d2d0d1877eb1c
Kushmy <31040087+Kushmy@users.noreply.github.com>
2019-05-06 02:02:50 +0530

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,15 @@
+package guvi;
+import java.util.Scanner;
+public class hello {
+	public static void main(String[] args)
+	{
+		int n;
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt();
+		for(int i=0;i<n;i++)
+		{
+			System.out.println("Hello");
+		}
+	}
+
+}
