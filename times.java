0aa858c894137dd0deb7ac851da9c419446af30e
Kushmy <31040087+Kushmy@users.noreply.github.com>
2019-05-06 02:05:24 +0530

    Add files via upload

---
+++ times.java
@@ -0,0 +1,14 @@
+package guvi;
+import java.util.Scanner;
+public class times {
+	public static void main(String[] args)
+	{
+		int n;
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt();
+		for(int i=0;i<n;i++)
+		{
+			System.out.println("Hello");
+		}
+	}
+}
