2248a695e1b299bb621bb3acc0a306df517a225b
Kushmy <31040087+Kushmy@users.noreply.github.com>
2019-05-13 23:17:17 +0530

    Add files via upload

---
+++ multiples.java
@@ -0,0 +1,14 @@
+package guvi1;
+import java.util.Scanner;
+public class multiples {
+	public static void main(String[] args)
+	{
+	int num;
+	Scanner sc=new Scanner(System.in);
+	num=sc.nextInt();
+	for(int i=1;i<=5;i++)
+		System.out.println(i*num);
+		
+	}
+
+}
